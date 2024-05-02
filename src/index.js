f56c49c4a386eab917f24468f9fcc66b85fb611e
fermioko <mioko.senac.2023@gmail.com>
2024-05-02 16:08:45 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -10,11 +10,22 @@ const db = require('./db'); //IMPORTANDO O NOSSO MÓDULO DE CONEXÃÕ COM O BANC
 const app = express();
 //O APP IRÁ RECEBER O EXPRESS E TODAS SUAS DEPENDÊNCIAS
 
+const cors = require('cors');
+
 //** Configuração das rotas **/
 const routes = require('./routes'); //CHAMANDO O MÓDULO DAS ROTAS
 
 //**INCLUIR NOVO **//
 app.use(express.json()); // AQUI TRANSFORMAMOS OS DADOS QUE CHEGAM COMO BINARIO EM JSON
+
+app.use((req, res, next) =>{
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers: Content-Type')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
+
+    app.use(cors())
+    next()
+})
 
 app.use('/', routes);
 //APÓS DECLARAR NOSSAS ROTAS, AQUI FALAMOS PARA NOSSO APP USAR ELAS COMO REFERÊNCIA
